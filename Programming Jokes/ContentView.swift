92787af978b238442c9d6cceac987696d8bd2710
Ryan Lim <rlim@bsd220.org>
2022-02-01 11:17:25 -0600

    Finished Setting Up Alert

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(jokes) { joke in
@@ -24,10 +25,32 @@ struct ContentView: View {
         .onAppear(perform: {
             getJokes()
         })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
     }
     func getJokes() {
         let apiKey = "?rapidapi-key=(b1a00b1328msh10239c7c9cf6888p1e559ejsnb2c1624f88ee)"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = Joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                    return
+                }
+            }
         }
+        showingAlert = true
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
